e4f6aeaea9780882721db569222c8a03ab9e403a
LugpDev2022 <lugpdev2022@gmail.com>
2025-01-29 20:13:25 -0600

    product model

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Node API');
 });
+
+mongoose
+  .connect(
+    'mongodb+srv://lugpdev2022:hwQNw5SKTqzTWuto@backenddb.nj4l0.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB'
+  )
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
